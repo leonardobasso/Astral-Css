ac664eba820481d9f84a42ed47777fe6754cf91d
sinistraunita <parliamodiarte1@gmail.com>
2020-09-16 20:04:00 +0200

    menu su mobile funzionante

--- static/script.js
+++ static/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $('.burger').click(function (e) { 
         $('.navbar').toggleClass('burger-close');
+        $('.nav-items').toggleClass('menu-open');
+
         console.log('ciao');
         e.preventDefault();
         
